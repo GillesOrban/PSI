d72c469dfd2d9f4369a6f45b695e33731ff47543
Gilles Orban de Xivry <gorban@ulg.ac.be>
2023-06-16 16:08:38 +0200

    Kernel sensor improvement: - modal basis improvements - loop method - saving loop statistics - evaluation of metrics in line with PsiSensor - display results

--- psi/kernelSensor.py
+++ psi/kernelSensor.py
@@ -36,11 +36,12 @@ import importlib
 import psi.psi_utils as psi_utils
 from .configParser import loadConfiguration
 from .instruments import  CompassSimInstrument, DemoCompassSimInstrument, HcipySimInstrument
-from .helperFunctions import LazyLogger, timeit, build_directory_name, copy_cfgFileToDir
+from .helperFunctions import LazyLogger, timeit, build_directory_name, \
+    copy_cfgFileToDir, dump_config_to_text_file
 
 from astropy.visualization import imshow_norm,\
     SqrtStretch, MinMaxInterval, PercentileInterval, \
-    LinearStretch, SinhStretch, LogStretch
+    LinearStretch, SinhStretch, LogStretch, ManualInterval
 
 
 # from config.config_metis_compass import conf
@@ -111,9 +112,49 @@ class KernelSensor():
 
 
         # Init logging buffer
+        self._ncpa_correction_long_term = 0
         self.iter = 0 # iteration index of PSI
         self._loop_stats = []
 
+        # -- Plotting & saving results
+        # self.fig = plt.figure(figsize=(9, 3))
+        if self.cfg.params.save_loop_statistics:
+            if self.cfg.params.save_dirname is None or self.cfg.params.save_dirname=='': 
+                self._directory = build_directory_name(self._config_file,
+                                                self.cfg.params.save_basedir)
+            else:
+                self._directory = self.cfg.params.save_basedir + self.cfg.params.save_dirname
+
+            if not os.path.exists(self._directory):
+                os.makedirs(self._directory)
+
+            # copy initial config file to directory
+            copy_cfgFileToDir(self._directory, self._config_file)
+
+            # copy current configuration to text file
+            dump_config_to_text_file(self._directory + '/config/' + 'current_config.txt',
+                                     self.cfg.params)
+
+            if self.cfg.params.save_phase_screens:
+                self._directory_phase = self._directory + 'residualNCPA/'
+                os.mkdir(self._directory_phase)
+
+            self.logger.info('Results will be stored in '
+                             '{0}'.format(self._directory))
+
+    def _save_loop_stats(self):
+        '''
+            Saving loop statistics to file
+
+            16/06/2023  copied from PsiSensor
+        '''
+        data = np.array(self._loop_stats)
+        np.savetxt(os.path.join(self._directory, 'loopStats.csv'),
+                  data,
+                  header ='units are nm \n it \t wfe_all_f \t wfe_qs_f \t wfe_all \t wfe_qs \t input_wv_avg \t wfe_all_f_avg \t wfe_static',
+                  fmt=['%i' , '%f', '%f', '%f', '%f', '%f', '%f', '%f'],
+                  delimiter= '\t')
+        
 
     def _buildModelKPI(self, fname="scexao_asym.fits.gz", verbose=False):
         '''
@@ -271,25 +312,33 @@ class KernelSensor():
         '''
         diam = 1
         radial_cutoff = False
-
+        nmode_shift = 3 
         self.smallGrid = hcipy.make_pupil_grid(self.cfg.params.asym_nsteps)
         #self.smallGrid = hcipy.make_pupil_grid(self.cfg.params.asym_nsteps - 1)
-        self.M2C_small = hcipy.make_zernike_basis(nbOfModes, diam,
+        self.M2C_small = hcipy.make_zernike_basis(nbOfModes+nmode_shift, diam,
                                                    self.smallGrid,
-                                                  4,
+                                                  1,
                                                   radial_cutoff=radial_cutoff)
-        self.M2C_small = psi_utils.reorthonormalize(self.M2C_small, self._small_aperture.flatten())
+        mask = self._small_aperture.flatten()
+        # mask[mask!=0] =1
+        self.M2C_small = psi_utils.reorthonormalize(self.M2C_small,
+                                                    mask)
+        self.M2C_matrix_small = self.M2C_small.transformation_matrix[:, nmode_shift:]
         # TM = self.M2C_small.transformation_matrix.copy()
-        self.C2M_small = hcipy.inverse_tikhonov(self.M2C_small.transformation_matrix, 1e-3)
+        self.C2M_small = hcipy.inverse_tikhonov(self.M2C_small.transformation_matrix,
+                                                1e-3)[nmode_shift:,:]
 
-        self.M2C_large = hcipy.make_zernike_basis(nbOfModes, diam,
-                                                  self.inst.pupilGrid, 4,
+        self.M2C_large = hcipy.make_zernike_basis(nbOfModes+nmode_shift, diam,
+                                                  self.inst.pupilGrid, 1,
                                                   radial_cutoff=radial_cutoff)
         # binary_aperture = np.copy(self.inst.aperture)
         # binary_aperture[self.inst.aperture >=0.5] = 1
         # binary_aperture[self.inst.aperture < 0.5] = 0
-        self.M2C_large = psi_utils.reorthonormalize(self.M2C_large, self.inst.aperture)
-        self.C2M_large =hcipy.inverse_tikhonov(self.M2C_large.transformation_matrix, 1e-3)
+        self.M2C_large = psi_utils.reorthonormalize(self.M2C_large,
+                                                    self.inst.aperture)
+        self.M2C_matrix_large = self.M2C_large.transformation_matrix[:, nmode_shift:]
+        self.C2M_large =hcipy.inverse_tikhonov(self.M2C_large.transformation_matrix,
+                                               1e-3)[nmode_shift:,:]
 
     def _modalFilteringOnEP(self, ncpa_estimate):
         '''
@@ -298,7 +347,8 @@ class KernelSensor():
             TODO: uniformize method namings
         '''
         ncpa_modes      = self.C2M_large.dot(ncpa_estimate.flatten() * self.inst.aperture.flatten())
-        ncpa_estimate  = self.M2C_large.transformation_matrix.dot(ncpa_modes)
+        # ncpa_estimate  = self.M2C_large.transformation_matrix.dot(ncpa_modes)
+        ncpa_estimate  = self.M2C_matrix_large.dot(ncpa_modes)
 
         return ncpa_estimate, ncpa_modes
 
@@ -307,11 +357,12 @@ class KernelSensor():
             TODO: clarify method naming
         '''
         ncpa_modes      = self.C2M_small.dot(ncpa_estimate.flatten() * proj_mask.flatten())
-        ncpa_estimate  = self.M2C_large.transformation_matrix.dot(ncpa_modes)
+        ncpa_estimate  = self.M2C_matrix_large.dot(ncpa_modes)
 
         return ncpa_estimate, ncpa_modes
 
-    def next(self, display=True, check=False, gain=0.5, leak=0.9, integrator=True):
+    def next(self, display=True, check=False, leak=1, integrator=True):
+        # TODO replace psi_framerate by kernel_framerate
         nbOfSeconds = 1/self.cfg.params.psi_framerate
         science_images_buffer = self.inst.grabScienceImages(nbOfSeconds)
 
@@ -319,36 +370,170 @@ class KernelSensor():
         self.computeWavefront(self.science_image)
         self._ncpa_estimate, _ = self._projectOnModalBasis(self.wavefront, self._small_aperture)
 
+        gain = self.cfg.params.gain_I
         self._ncpa_command = - gain * self._ncpa_estimate
 
-        self.inst.setNcpaCorrection(self._ncpa_command, integrator=integrator, leak=leak)
+        self.inst.setNcpaCorrection(self._ncpa_command,
+                                    phase_prop=self.cfg.params.gain_P * self._ncpa_command,
+                                    integrator=integrator, leak=leak)
         self.iter +=1
 
+        if display:
+            self.show()
+
+
+    def show(self):
+        plt.clf()
+
+        # plt.subplot(141)
+        # imshow_norm(img.mean(0), stretch=LogStretch())
+        ax1 = plt.subplot(141)
+        im1, _= imshow_norm(self.science_image, stretch=LogStretch(), ax=ax1)
+        # plt.subplot(142)
+        # inter = PercentileInterval(99.5)
+        vmin = np.percentile(self.inst.phase_wv + self.inst.phase_ncpa, 1)
+        vmax = np.percentile(self.inst.phase_wv + self.inst.phase_ncpa, 99)
+        inter = ManualInterval(vmin, vmax)
+        ax2 = plt.subplot(142)
+        im2, _=imshow_norm((self.inst.phase_wv + self.inst.phase_ncpa).shaped,
+                           interval=inter, ax=ax2)
+        # plt.subplot(143)
+        # imshow_norm(self._ncpa_estimate.reshape((256, 256)) * self.inst.aperture.shaped, interval=inter)
+        ax3 = plt.subplot(143)
+        im3, _=imshow_norm(-self.inst.phase_ncpa_correction.reshape((256, 256)) * self.inst.aperture.shaped,
+                           interval=inter, ax=ax3)
+        # plt.subplot(144)
+        ax4 = plt.subplot(144)
+        im4, _=imshow_norm(self.inst.aperture.shaped *
+                           (self.inst.phase_wv + self.inst.phase_ncpa +
+                           self.inst.phase_ncpa_correction).shaped,
+                           interval=inter, ax=ax4)
+
+        ax1.set_axis_off()
+        ax2.set_axis_off()
+        ax3.set_axis_off()
+        ax4.set_axis_off()
+        ax2.set_title('static NCPA + WV')
+        ax3.set_title('NCPA correction')
+        ax4.set_title('Residuals')
+        plt.tight_layout()
+        plt.draw()
+        plt.pause(0.01)
+        time.sleep(0.1)
+        #ims.append([im1,im2, im3, im4])
+
+    def loop(self, **kwargs):
+        # TODO replace psi_nb_iter by an agnostic 'nb_iter'
+        for i in range(self.cfg.params.psi_nb_iter):
+            self.next(**kwargs)
+            self.evaluateSensorEstimate()
+            # if self.cfg.params.save_phase_screens:
+            #     self._store_phase_screens_to_file(self.iter)
+
+        if self.cfg.params.save_loop_statistics:
+            self._save_loop_stats()
+
+    # def evaluateSensorEstimate(self, verbose=True):
+    #     res_ncpa_qs = self.inst.phase_ncpa + self.inst.phase_ncpa_correction
+    #     res_ncpa_all = self.inst.phase_ncpa + self.inst.phase_wv + \
+    #         self.inst.phase_ncpa_correction
+    #     # Residual over the average WV over one iteration (/ detector integration)
+    #     res_ncpa_all_bis = self.inst.phase_ncpa + self.inst.phase_wv_integrated + \
+    #         self.inst.phase_ncpa_correction
+
+    #     conv2nm = self.inst.wavelength / (2 * np.pi) * 1e9
+
+    #     # TODO: For asymmetric Lyot, maybe we want to use lyot stop instead of EP
+    #     rms_res_qs = np.std(res_ncpa_qs[self.inst.aperture>=0.5]) * conv2nm
+    #     rms_res_all = np.std(res_ncpa_all[self.inst.aperture>=0.5]) * conv2nm
+    #     rms_res_all_bis = np.std(res_ncpa_all_bis[self.inst.aperture>=0.5]) * conv2nm
+
+    #     # rms residual errors
+    #     tmp, _ = self._modalFilteringOnEP(res_ncpa_qs)
+    #     rms_res_qs_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+    #     tmp, _ = self._modalFilteringOnEP(res_ncpa_all)
+    #     rms_res_all_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+    #     tmp, _ = self._modalFilteringOnEP(res_ncpa_all_bis)
+    #     rms_res_all_bis_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+
+    #     # rms WV perturbations and
+    #     tmp, _ = self._modalFilteringOnEP(self.inst.phase_wv)
+    #     rms_wv = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+    #     tmp, _ = self._modalFilteringOnEP(self.inst.phase_wv_integrated)
+    #     rms_wv_integrated = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+
+    #     tmp, _ = self._modalFilteringOnEP(self.inst.phase_ncpa_correction)
+    #     rms_corr = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+
+    #     tmp, _ = self._projectOnModalBasis(res_static_ncpa_qs)
+    #     rms_res_static_NCPA_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+
+
+    #     if verbose:
+    #         self.logger.info('\n#{0} : Res [QS, QS+WV,  QS+WV b] = [{1:.0f}, {2:.0f}, {3:.0f}]'.\
+    #             format(self.iter, rms_res_qs, rms_res_all, rms_res_all_bis))
+    #         self.logger.info('#{0} : Res. modal sfilt. [QS, QS+WV, QS+WV b] = [{1:.0f}, {2:.0f}, {3:.0f}]'.\
+    #             format(self.iter, rms_res_qs_filt, rms_res_all_filt, rms_res_all_bis_filt))
+    #         self.logger.info('#{0} : Input WV_f rms (last, integrated)  = ({1:.0f}, {2:.0f})'.format(self.iter, rms_wv, rms_wv_integrated))
+    #         self.logger.info('#{0} : Sensor correction rms = {1:.0f}'.format(self.iter, rms_corr))
+
+
+    #     loop_stat = [self.iter]
+    #     loop_stat.append(rms_res_all_filt)
+    #     loop_stat.append(rms_res_qs_filt)
+    #     loop_stat.append(rms_res_all)
+    #     loop_stat.append(rms_res_qs)
+    #     # [01/07/2022] : added 01/07/2022
+    #     loop_stat.append(rms_wv_integrated)  # input WV average over 1/psi_framertae -- on the modes
+    #     loop_stat.append(rms_res_all_bis_filt)    # rms all considering the average WV and not the instantaneoius
+    #     loop_stat.append(rms_res_static_NCPA_filt)  # long-term average of the correction compared to the QS part
+    #     self._loop_stats.append(loop_stat)
 
     def evaluateSensorEstimate(self, verbose=True):
+        '''
+            Compute the rms errors made on quasi-static NCPA and on water vapour seeing.
+
+            /!\ Only valid for a `CompassSimInstrument` and `DemoCompassSimInstrument`
+
+            TODO make it generic to any instruments
+        '''
         res_ncpa_qs = self.inst.phase_ncpa + self.inst.phase_ncpa_correction
         res_ncpa_all = self.inst.phase_ncpa + self.inst.phase_wv + \
             self.inst.phase_ncpa_correction
-        # Residual over the average WV over one iteration (/ detector integration)
+        # 2022-06-2x ...
+        if self.iter == 0:
+            res_static_ncpa_qs = self.inst.phase_ncpa
+        else:
+            # tmp_avg = np.mean(self.inst.phase_ncpa_correction[self.inst.aperture>=0.5])
+            self._ncpa_correction_long_term += self.inst.phase_ncpa_correction #- tmp_avg)
+            # self._ncpa_correction_long_term /= self.iter
+
+            res_static_ncpa_qs = self.inst.phase_ncpa + (self._ncpa_correction_long_term / self.iter)
+
+        # 2022-07-01 -- metric with the average WV over one iteration
         res_ncpa_all_bis = self.inst.phase_ncpa + self.inst.phase_wv_integrated + \
             self.inst.phase_ncpa_correction
 
         conv2nm = self.inst.wavelength / (2 * np.pi) * 1e9
-
-        # TODO: For asymmetric Lyot, maybe we want to use lyot stop instead of EP
+        # rms_input_qs = np.std(self.inst.phase_ncpa[self.inst.aperture==1]) * conv2nm
+        # rms_input_all = np.std((self.inst.phase_ncpa + \
+        #                         self.inst.phase_wv)[self.inst.aperture==1]) * conv2nm
         rms_res_qs = np.std(res_ncpa_qs[self.inst.aperture>=0.5]) * conv2nm
         rms_res_all = np.std(res_ncpa_all[self.inst.aperture>=0.5]) * conv2nm
         rms_res_all_bis = np.std(res_ncpa_all_bis[self.inst.aperture>=0.5]) * conv2nm
 
-        # rms residual errors
-        tmp, _ = self._modalFilteringOnEP(res_ncpa_qs)
-        rms_res_qs_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
-        tmp, _ = self._modalFilteringOnEP(res_ncpa_all)
-        rms_res_all_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
-        tmp, _ = self._modalFilteringOnEP(res_ncpa_all_bis)
-        rms_res_all_bis_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
 
-        # rms WV perturbations and
+        if self.cfg.params.psi_correction_mode is not 'all':
+            tmp, _ = self._modalFilteringOnEP(res_ncpa_qs)
+            rms_res_qs_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+            tmp, _ = self._modalFilteringOnEP(res_ncpa_all)
+            rms_res_all_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+            tmp, _ = self._modalFilteringOnEP(res_ncpa_all_bis)
+            rms_res_all_bis_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+        else:
+            rms_res_qs_filt = rms_res_qs
+            rms_res_all_filt = rms_res_all
+
         tmp, _ = self._modalFilteringOnEP(self.inst.phase_wv)
         rms_wv = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
         tmp, _ = self._modalFilteringOnEP(self.inst.phase_wv_integrated)
@@ -357,13 +542,17 @@ class KernelSensor():
         tmp, _ = self._modalFilteringOnEP(self.inst.phase_ncpa_correction)
         rms_corr = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
 
+        tmp, _ = self._modalFilteringOnEP(res_static_ncpa_qs)
+        rms_res_static_NCPA_filt = np.std(tmp[self.inst.aperture>=0.5]) * conv2nm
+
         if verbose:
-            self.logger.info('\n#{0} : Res [QS, QS+WV,  QS+WV b] = [{1:.0f}, {2:.0f}, {3:.0f}]'.\
+            self.logger.info('#{0} : Res [QS, QS+WV,  QS+WV b] = [{1:.0f}, {2:.0f}, {3:.0f}]'.\
                 format(self.iter, rms_res_qs, rms_res_all, rms_res_all_bis))
-            self.logger.info('#{0} : Res. modal sfilt. [QS, QS+WV, QS+WV b] = [{1:.0f}, {2:.0f}, {3:.0f}]'.\
+            self.logger.info('#{0} : Res. filt. [QS, QS+WV, QS+WV b] = [{1:.0f}, {2:.0f}, {3:.0f}]'.\
                 format(self.iter, rms_res_qs_filt, rms_res_all_filt, rms_res_all_bis_filt))
-            self.logger.info('#{0} : Input WV_f rms (last, integrated)  = ({1:.0f}, {2:.0f})'.format(self.iter, rms_wv, rms_wv_integrated))
-            self.logger.info('#{0} : Sensor correction rms = {1:.0f}'.format(self.iter, rms_corr))
+            self.logger.info('#{0} : input WV_f rms (last, integrated)  = ({1:.0f}, {2:.0f})'.format(self.iter, rms_wv, rms_wv_integrated))
+            self.logger.info('#{0} : PSI correction rms = {1:.0f}'.format(self.iter, rms_corr))
+            self.logger.info('#{0} : Long-term (static) residual rms = {1:.0f}'.format(self.iter, rms_res_static_NCPA_filt))
 
 
         loop_stat = [self.iter]
@@ -374,5 +563,5 @@ class KernelSensor():
         # [01/07/2022] : added 01/07/2022
         loop_stat.append(rms_wv_integrated)  # input WV average over 1/psi_framertae -- on the modes
         loop_stat.append(rms_res_all_bis_filt)    # rms all considering the average WV and not the instantaneoius
-        # loop_stat.append(rms_res_static_NCPA_filt)  # long-term average of the correction compared to the QS part
+        loop_stat.append(rms_res_static_NCPA_filt)  # long-term average of the correction compared to the QS part
         self._loop_stats.append(loop_stat)
